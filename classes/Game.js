998b55370e70fceb6b4dd9b6dc0d678da1627978
Nik Legault <50716413+niklegault@users.noreply.github.com>
2024-01-02 17:01:14 -0500

    Basic class definitions

---
+++ classes/Game.js
@@ -0,0 +1,12 @@
+
+/**
+ * Represents a game, holds information about the 2 teams competing, such as:
+ * goals and shots (includes normal, high-danger, and breakaway)
+ *
+ * @class
+ * @author Nik Legault
+ * @version 0.0.1
+ */
+class Game {
+
+}
---
+++ classes/Goalie.js
@@ -0,0 +1,12 @@
+
+/**
+ * Represents a goalie, holding information about games, saves, goals allowed,
+ * and other more advanced stats
+ *
+ * @class
+ * @author Nik Legault
+ * @version 0.0.1
+ */
+class Goalie {
+
+}
